7fb59edc1ab02adfc7f9227787035677171b3868
rudransh <rudransh9shinghal@gmail.com>
2024-04-23 16:25:09 +0530

    the final contract

--- first-token/examples/schema.rs
+++ first-token/examples/schema.rs
@@ -3,8 +3,6 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use first_token::msg::{CustomResponse, ExecuteMsg, InstantiateMsg, QueryMsg};
-use first_token::state::State;
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
@@ -12,9 +10,8 @@ fn main() {
     create_dir_all(&out_dir).unwrap();
     remove_schemas(&out_dir).unwrap();
 
-    export_schema(&schema_for!(InstantiateMsg), &out_dir);
-    export_schema(&schema_for!(ExecuteMsg), &out_dir);
-    export_schema(&schema_for!(QueryMsg), &out_dir);
-    export_schema(&schema_for!(State), &out_dir);
-    export_schema(&schema_for!(CustomResponse), &out_dir);
+    export_schema(&schema_for!(cw20_base::msg::ExecuteMsg), &out_dir);
+    export_schema(&schema_for!(cw20_base::msg::InstantiateMsg), &out_dir);
+    export_schema(&schema_for!(cw20_base::msg::QueryMsg), &out_dir);
+    
 }
