a258e0173d51250c906d8bdc50677d7be8bfcb6b
B1smuth <siddharthaswarnkar@gmail.com>
2024-04-25 13:20:43 +0530

    All files added and working

--- first-token/examples/schema.rs
+++ first-token/examples/schema.rs
@@ -3,8 +3,7 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use first_token::msg::{CustomResponse, ExecuteMsg, InstantiateMsg, QueryMsg};
-use first_token::state::State;
+use cw20_base::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
@@ -15,6 +14,4 @@ fn main() {
     export_schema(&schema_for!(InstantiateMsg), &out_dir);
     export_schema(&schema_for!(ExecuteMsg), &out_dir);
     export_schema(&schema_for!(QueryMsg), &out_dir);
-    export_schema(&schema_for!(State), &out_dir);
-    export_schema(&schema_for!(CustomResponse), &out_dir);
 }
